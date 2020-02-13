81f985198a7165c0524566159d0e128cf67aabde
Milan Nagra <a01168827@se14l0120-19.ad.bcit.ca>
2020-02-13 14:03:49 -0800

    added to milan.js

--- milan.js
+++ milan.js
@@ -1,1 +1,4 @@
-//Milan's Branch js file 
+//Milan's Branch js file
+//Eminem has just become the first celebrity to be diagnosed with Coronavirus. In a statement released by doctors, 
+//it has been revealed that his palms were sweaty, knees weak and
+// arms were heavy. He presented with vomit on his sweater already. Initial testing has revealed it was mums spaghetti.
